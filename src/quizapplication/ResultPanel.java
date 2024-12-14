885408170941142f65857388346d9678fc43024c
nnguyenbon <nguyenbaongyn@gmail.com>
2024-12-14 19:57:49 +0700

    update

--- src/quizapplication/ResultPanel.java
+++ src/quizapplication/ResultPanel.java
@@ -3,10 +3,31 @@
  */
 package quizapplication;
 
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
 /**
  *
  * @author Nguyen Kim Bao Nguyen - CE191239
  */
 public class ResultPanel {
+    private MainFrame mainFrame;
+    private JLabel resultLabel;
+    private JButton restartButton;
     
+    public ResultPanel(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+        mainFrame.setLayout(new BorderLayout());
+        
+        resultLabel = new JLabel("Score ");
+        mainFrame.add(resultLabel, BorderLayout.CENTER);
+        
+        restartButton = new JButton("Restart");
+        mainFrame.add(restartButton, BorderLayout.SOUTH);
+    }
+    
+    public void setResult(int score) {
+        resultLabel.setText("I know you will get: " + score + "!");
+    }
 }
